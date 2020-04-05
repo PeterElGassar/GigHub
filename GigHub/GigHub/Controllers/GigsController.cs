5097cc20b71a312a9ee1ebcff8ac7aca01ebb425
PeterElGassar <PeterElGassar@users.noreply.github.com>
2020-04-05 15:41:37 +0200

    Create Add a Gig Form

---
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -0,0 +1,24 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+            public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var GenresList = _context.Genres.ToList();
+            GigFormViewModel model = new GigFormViewModel();
+            model.Genres = GenresList;
+            return View(model);
+        }
+    }
+}
---
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GigHub.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Location { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int GenreId { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+
+
+    }
+}
